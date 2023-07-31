22a976cb4c8e23cac3aaee3b23910b2f1a62be22
TheDaidor2001 <danielcastillo1552001@hotmail.com>
2023-07-31 13:03:11 +0100

    fixed cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,7 +42,7 @@ onMounted(async () => {
           <vLoading 
           />
       </div>
-      <div class="mt-10 grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-4 gap-10 w-2/3 mx-auto">
+      <div class="mt-10 grid grid-cols-1 md:grid-cols-2 3xl:grid-cols-4 gap-10 px-5 lg:px-0 w-full lg:w-2/3 mx-auto">
        <vProduct 
         v-for="producto in usb.productos"
         :producto="producto"
